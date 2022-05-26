962a573350acc894e411efde2602b6749065dbed
jt3020 <jt3020@ic.ac.uk>
2022-05-26 12:11:24 +0100

    Added CRS module

---
+++ src/centralproc/CRS.f90
@@ -0,0 +1,11 @@
+Module CRS_Mod
+
+    use Constants_Mod
+    !!Compressed row storage module which handles the optimal data storage of sparse matrices for the problem
+
+contains
+
+
+
+
+End Module
